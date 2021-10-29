b817e84ba6154a7b9616e12c68c71a22c43a9d96
Ale <alearanibar@gmail.com>
2021-10-29 11:35:53 -0700

    project complete

--- src/components/AddToCart.tsx
+++ src/components/AddToCart.tsx
@@ -4,7 +4,7 @@ import { CartItem, useStateDispatch } from "./AppState";
 export interface AddToCartProps {
   addToCart: (item: Omit<CartItem, "quantity">) => void;
 }
-export function withAddToCart<OriginalProps>(
+export function withAddToCart<OriginalProps extends AddToCartProps>(
   ChildComponent: React.ComponentType<OriginalProps>
 ) {
   const AddToCartHOC = (props: Omit<OriginalProps, keyof AddToCartProps>) => {
@@ -26,3 +26,31 @@ export function withAddToCart<OriginalProps>(
   };
   return AddToCartHOC;
 }
+
+export const WithAddToCartProps: React.FC<{
+  children: (props: AddToCartProps) => JSX.Element;
+}> = ({ children }) => {
+  const dispatch = useStateDispatch();
+  const addToCart: AddToCartProps["addToCart"] = (item) => {
+    dispatch({
+      type: "ADD_TO_CART",
+      payload: {
+        item,
+      },
+    });
+  };
+  return children({ addToCart });
+};
+
+export const useAddToCart = () => {
+  const dispatch = useStateDispatch();
+  const addToCart: AddToCartProps["addToCart"] = (item) => {
+    dispatch({
+      type: "ADD_TO_CART",
+      payload: {
+        item,
+      },
+    });
+  };
+  return addToCart;
+};
--- src/components/SpecialOffer.tsx
+++ src/components/SpecialOffer.tsx
@@ -1,33 +1,35 @@
 import React from "react";
 import { Pizza } from "./types";
-import { useStateDispatch } from "./AppState";
 import SpecialOfferCss from "./SpecialOffer.module.css";
+import { AddToCartProps, WithAddToCartProps } from "./AddToCart";
 
 interface Props {
   pizza: Pizza;
 }
 const SpecialOffer: React.FC<Props> = ({ pizza }) => {
-  const dispatch = useStateDispatch();
-  const handleAddToCartClick = () => {
-    dispatch({
-      type: "ADD_TO_CART",
-      payload: {
-        item: {
-          id: pizza.id,
-          name: pizza.name,
-          price: pizza.price,
-        },
-      },
-    });
-  };
   return (
     <div className={SpecialOfferCss.container}>
       <h2>{pizza.name}</h2>
       <p>{pizza.description}</p>
       <p>{pizza.price}</p>
-      <button type="button" onClick={handleAddToCartClick}>
-        Add to Cart
-      </button>
+      <WithAddToCartProps>
+        {({ addToCart }) => {
+          return (
+            <button
+              type="button"
+              onClick={() =>
+                addToCart({
+                  id: pizza.id,
+                  name: pizza.name,
+                  price: pizza.price,
+                })
+              }
+            >
+              Add to Cart
+            </button>
+          );
+        }}
+      </WithAddToCartProps>
     </div>
   );
 };
